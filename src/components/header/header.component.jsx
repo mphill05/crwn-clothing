60ca9b0b331741be4b1182d53492deb08382b2d2
Mark Phillips <mphill05@gmail.com>
2021-10-25 22:48:04 -0500

    created user and cart sagas for sign in, sign up and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
+import { signOutStart } from '../../redux/user/user.actions';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -17,7 +17,7 @@ import {
   OptionLink
 } from './header.styles';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
   <HeaderContainer>
     <LogoContainer to='/'>
       <Logo className='logo' />
@@ -29,16 +29,15 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to='/shop'>
         CONTACT
       </OptionLink>
-      {
-        currentUser ? (
-          <OptionLink as='div' onClick={() => auth.signOut()}>
-            SIGN OUT
-          </OptionLink>
-        ) : (
-          <OptionLink to='/signin'>
-            SIGN IN
-          </OptionLink>
-        )}
+      {currentUser ? (
+        <OptionLink as='div' onClick={signOutStart}>
+          SIGN OUT
+        </OptionLink>
+      ) : (
+        <OptionLink to='/signin'>
+          SIGN IN
+        </OptionLink>
+      )}
       <CartIcon />
     </OptionsContainer>
     {
@@ -52,4 +51,11 @@ const mapStateToProps = createStructuredSelector({
   hidden: selectCartHidden
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+  signOutStart: () => dispatch(signOutStart())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
